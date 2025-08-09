b17baec446535d17434df4c77478fd2362edd3ba
Aarush Kulkarni <ak4774@aarushs-laptop.mynetworksettings.com>
2025-08-09 17:41:12 -0400

    changed mentor and inspire

--- src/pages/JoinUs.jsx
+++ src/pages/JoinUs.jsx
@@ -90,7 +90,7 @@ export default function JoinUs() {
               />
               <PerkCard
                 title="Mentor and Inspire"
-                description="You will be able to inspire the younger generation through the problems which will introduce them to interesting parts of science."
+                description="You can show your passion for science and inspire younger students by helping to organize a competetive to help them grow more."
                 icon={<svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>}
                 tooltipText="Community Impact"
               />
