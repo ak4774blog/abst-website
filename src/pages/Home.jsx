53e8f6bfda62b6f8df988bd23726a8dd232d7cfb
Aarush Kulkarni <ak4774@aarushs-laptop.mynetworksettings.com>
2025-08-09 16:59:05 -0400

    more changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       <div className="max-w-4xl mx-auto px-4 py-0 sm:px-6 lg:px-8">
         <div className="text-lg text-slate-300 space-y-6 leading-relaxed">
           <p>
-            Welcome! ABST was made to create new opportunities for middle schoolers to explore and enjoy science through friendly competition
+            Welcome! ABST was made to create new opportunities for middle schoolers to explore and enjoy science through friendly competition.
           </p>
           <p className="p-6 bg-sky-900/50 border-l-4 border-sky-400 text-sky-200 rounded-r-lg">
             The Acton-Boxborough Science Tournament (ABST) is a 501(c)(3) nonprofit... We noticed that while there are plenty of math tournaments, science competitions for middle school students are much harder to find. We started ABST to help change that!
