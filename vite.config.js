4de1e9568ac5095b12f4167b776233a360fd2842
Aarush Kulkarni <ak4774@aarushs-laptop.mynetworksettings.com>
2025-08-19 08:33:03 -0400

    added sitemap generation for seo

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // <-- Must be imported
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap"; // Import the plugin
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- Must be included here in the plugins array
+    tailwindcss(),
+    Sitemap({ hostname: "https://absciencetournament.org" }), // Add the plugin
   ],
+  base: "/",
 });
