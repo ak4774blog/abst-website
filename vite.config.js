56f2e35d66ff0d73789c5ad72054657b0e0e15f5
Aarush Kulkarni <ak4774@aarushs-laptop.mynetworksettings.com>
2025-08-20 07:49:24 -0400

    fix dynamic routes again

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { sitemap } from 'vite-plugin-sitemap';
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
     // We are now adding the `dynamicRoutes` option to the sitemap plugin
-    sitemap({ 
+    Sitemap({ 
       hostname: 'https://absciencetournament.org',
       // This array explicitly lists all of your pages for the sitemap
       dynamicRoutes: [
